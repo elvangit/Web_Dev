ce4e9929cade0f13e521edea4b9625c6541a2d1d
elvangit <146006949+elvangit@users.noreply.github.com>
2023-12-07 00:36:25 +0700

    Update dasar-bot-class.py

--- dasar-bot-class.py
+++ dasar-bot-class.py
@@ -73,4 +73,4 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
-bot.run('MTE4MTI1NTc2OTkwODMyMjMxNA.Gmuzu2.VPPs4SO0ZXEJstVPVBXNyXGyYHFuOKzzYCYmNg')
+bot.run('TOKEN')
